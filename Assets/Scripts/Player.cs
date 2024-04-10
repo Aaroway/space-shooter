92099e0e966f7099aaa72440e88a3604e4a987e0
Aaroway <146600984+Aaroway@users.noreply.github.com>
2024-04-10 14:49:54 -0500

    player and laser movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 6;
+    public GameObject playerLaser;
+    private float _canfire = -1f;
+    private float _fireRate = .3f;
+    
+    //@enemies
+    void Start()
+    {
+        //take the current position and give it a start
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        FireLaser();
+
+        
+
+    }
+    void CalculateMovement()
+    {
+        //input controls
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //y boundary
+        if (transform.position.y >= 2)
+        {
+            transform.position = new Vector3(transform.position.x, 2, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //x boundary
+        if (transform.position.x >= 11.2f)
+        {
+            transform.position = new Vector3(-11.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.2f)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canfire)
+        {
+            _canfire = Time.time + _fireRate;
+            Instantiate(playerLaser, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+    }
+}
