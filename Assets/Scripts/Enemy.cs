5c5d6a02183dd61038ba78b1a50ef219fb6299db
Aaroway <146600984+Aaroway@users.noreply.github.com>
2024-04-10 17:35:39 -0500

    enemy hit detection

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _enemySpeed = 4.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        EnemyMovement();
+    }
+
+    void EnemyMovement()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if (transform.position.y < -5.5f)
+        {
+            float randomX = Random.Range(-11, 11);
+            transform.position = new Vector3(randomX, 8, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit: " + other.transform.name);
+    }
+}
